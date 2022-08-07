08297d51f63a483190d025b8b5fe8106a9ea6fd9
yoni kremer <yoni.kremer@gmail.com>
2022-08-07 15:24:54 +0300

    Added an example to example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+"""Example of how to use the program."""
+
+
+import os
+
+
+def main():
+    """The function called when running the file example.py"""
+    example_url = "https://www.python.org/"
+    os.system(f"python link_back.py {example_url} 9")
+
+
+if __name__ == '__main__':
+    main()
